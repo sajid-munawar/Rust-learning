ba5689ae2f8bafd43bc23679c31d6fe996f7a55e
sajid-munawar <sajidmunawar666@gmail.com>
2021-01-24 17:02:42 +0500

    const and its type

---
+++ const_1/target/debug/const_1.d
@@ -0,0 +1,1 @@
+/home/sajid/Rust-learning.git/const_1/target/debug/const_1: /home/sajid/Rust-learning.git/const_1/src/main.rs
---
+++ const_1/target/debug/deps/const_1-53848ff53c14f270.d
@@ -0,0 +1,5 @@
+/home/sajid/Rust-learning.git/const_1/target/debug/deps/const_1-53848ff53c14f270: src/main.rs
+
+/home/sajid/Rust-learning.git/const_1/target/debug/deps/const_1-53848ff53c14f270.d: src/main.rs
+
+src/main.rs:
