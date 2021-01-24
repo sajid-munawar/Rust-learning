713bfca44a318aa19063b61d9e282f9273f7a3e3
sajid-munawar <sajidmunawar666@gmail.com>
2021-01-24 15:08:08 +0500

    added readme.md and cargo check

---
+++ hello_world/src/readme.md
@@ -0,0 +1,5 @@
+crete a project with
+         `cargo new hello_world`
+then open the project in vs code
+then `cltr+backtick` to open terminal in vs code
+then        `cargo check` 
---
+++ hello_world/target/debug/deps/hello_world-02af914aecad81c8.d
@@ -0,0 +1,5 @@
+/home/sajid/Rust-learning.git/hello_world/target/debug/deps/hello_world-02af914aecad81c8.rmeta: src/main.rs
+
+/home/sajid/Rust-learning.git/hello_world/target/debug/deps/hello_world-02af914aecad81c8.d: src/main.rs
+
+src/main.rs:
