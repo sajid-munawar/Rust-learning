8c5f51f81d8779dc5e6d60b1fb268c3e829cae74
sajid-munawar <sajidmunawar666@gmail.com>
2021-02-03 23:14:29 +0500

    cannot assign twice to immutable variable

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,10 @@
 fn main() {
+    let age = 50;
+    age = 55  //error: cannot assign twice to immutable variable
+    // so we will use mut keyword
     let mut temperature =35;
     temperature=40;
-    println!("{}",temperature) 
+    println!("{}",temperature) ;
 }
 
 // println!("{}",temperature) 
