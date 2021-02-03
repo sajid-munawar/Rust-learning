cefde49a0959b153515c7b9310cc677d35e33945
sajid-munawar <sajidmunawar666@gmail.com>
2021-02-03 22:51:51 +0500

    signed unsigned integer

--- signed_unsigned_int/src/main.rs
+++ signed_unsigned_int/src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
+    // u for unsigned integer and i for signed integer
     let number:i32= 80;
     println!("{}",number);
-    let number:i32= -80;
+    // let number:u32= -80;
     println!("{}",number);
     let number:i32= 8996;
     println!("{}",number);
