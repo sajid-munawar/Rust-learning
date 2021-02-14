5436271bbaeb78c333b6eea4d3b534b019a2e0e8
sajid-munawar <sajidmunawar666@gmail.com>
2021-02-14 15:31:22 +0500

    infer data types of tuple values

--- compound_data_types/src/main.rs
+++ compound_data_types/src/main.rs
@@ -1,19 +1,18 @@
 fn main() {
-    let arr:[u32;5]=[2,4,5,6,7];
-    println!("{}",arr[1]);
-//  let student=(22,9,35,'a',2.5);
-// println!("{}",student.0);
-// println!("{}",student.1);
-// println!("{}",student.2);
-// println!("{}",student.3);
-// println!("{}",student.4);
-// println!("destructering");
-// let (a,b,c,d,e)=student;
-// println!("{}",a);
-// println!("{}",b);
-// println!("{}",c);
-// println!("{}",d);
-// println!("{}",e);
-// println!("{}","end");
+
+ let student:(u32,u32,u32,char,f64)=(22,9,35,'a',2.5);
+println!("{}",student.0);
+println!("{}",student.1);
+println!("{}",student.2);
+println!("{}",student.3);
+println!("{}",student.4);
+println!("destructering");
+let (a,b,c,d,e)=student;
+println!("{}",a);
+println!("{}",b);
+println!("{}",c);
+println!("{}",d);
+println!("{}",e);
+println!("{}","end");
 }
  
